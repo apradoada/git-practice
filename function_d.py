d9e64ad851b20ca0d2aae2c1529d3ddfd9b80f96
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2025-07-11 16:53:38 -0500

    Completed Function d

--- function_d.py
+++ function_d.py
@@ -2,8 +2,11 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
-
+    maxi = 0
+    for i in range(len(numbers)):
+        maxi = max(maxi, i)
+    print("hello")
+    return maxi
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
